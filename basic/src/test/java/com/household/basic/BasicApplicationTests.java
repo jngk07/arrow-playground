9a914cc2c4f5985eb11d461cb3e04d5482b151c6
jngk07 <jngk07@gmail.com>
2025-05-28 22:29:38 -0500

    basic folders

---
+++ basic/src/test/java/com/household/basic/BasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.household.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
